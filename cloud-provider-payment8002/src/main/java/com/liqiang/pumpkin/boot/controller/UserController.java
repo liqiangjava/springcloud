7ef0be12dc1ef959656c8a606a6bf109e54b0476
李强 <qiangli@y1cloud.com>
2020-04-04 20:26:32 +0800

    关闭自我保护机制

--- cloud-provider-payment8002/src/main/java/com/liqiang/pumpkin/boot/controller/UserController.java
+++ cloud-provider-payment8002/src/main/java/com/liqiang/pumpkin/boot/controller/UserController.java
@@ -5,6 +5,8 @@ import com.liqiang.pumpkin.boot.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -25,12 +28,26 @@ public class UserController {
   @Value("${server.port}")
   private String serverPort;
 
+  @Autowired
+  private DiscoveryClient discoveryClient;
+
   @GetMapping("/{id}")
   public ResponseEntity findById(@PathVariable Long id) {
     User findOne = userRepository.getOne(id);
     Map<String, Object> map = new HashMap<>();
     map.put("user", findOne);
     map.put("port", serverPort);
+    List<String> services = discoveryClient.getServices();
+    for (String str : services) {
+      log.info("elements:{}", str);
+    }
+    map.put("services", services);
+    List<ServiceInstance> serviceInstances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+    for (ServiceInstance serviceInstance : serviceInstances) {
+      log.info("serviceId:{},host:{},port:{},uri:{}", serviceInstance.getServiceId(), serviceInstance.getHost(),
+          serviceInstance.getPort(), serviceInstance.getUri());
+    }
+    map.put("serviceInstances", serviceInstances);
     return ResponseEntity.ok(map);
   }
 

e3c7e94dd5ce02bc77095dee5900eeaea8f621c4
李强 <qiangli@y1cloud.com>
2020-04-04 15:07:13 +0800

    添加提供者8002

---
+++ cloud-provider-payment8002/src/main/java/com/liqiang/pumpkin/boot/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.liqiang.pumpkin.boot.controller;
+
+import com.liqiang.pumpkin.boot.dao.UserRepository;
+import com.liqiang.pumpkin.boot.entity.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@Slf4j
+public class UserController {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Value("${server.port}")
+  private String serverPort;
+
+  @GetMapping("/{id}")
+  public ResponseEntity findById(@PathVariable Long id) {
+    User findOne = userRepository.getOne(id);
+    Map<String, Object> map = new HashMap<>();
+    map.put("user", findOne);
+    map.put("port", serverPort);
+    return ResponseEntity.ok(map);
+  }
+
+  @PostMapping("/create")
+  public User create(@RequestBody User user) {
+
+    return userRepository.save(user);
+  }
+}

9366bfcbcce6f529031975a756bc77141ed8aab1
李强 <qiangli@y1cloud.com>
2020-04-04 09:30:11 +0800

    添加eureka集群,向注册中心注册自己,并维护服务实例去检索服务

---
+++ cloud-eureka-server7003/src/main/java/com/liqiang/pumpkin/boot/Start.java
@@ -0,0 +1,15 @@
+package com.liqiang.pumpkin.boot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@EnableEurekaServer
+public class Start {
+    public static void main(String[] args) {
+        SpringApplication.run(Start.class, args);
+    }
+}
